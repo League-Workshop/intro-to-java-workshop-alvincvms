c87f204118723865b0e3a749cb35a21afef9dd90
alvincvms <48074117+alvincvms@users.noreply.github.com>
2019-03-28 18:28:48 -0700

    SEC 5
    :accept:
    maze

--- src/greeter.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JOptionPane;
-public class greeter {
-public static void main(String[] args) {
-	for (int derp = 0; derp < 5; derp++) {
-	JOptionPane.showMessageDialog(null, );
-	}
-}
-}
